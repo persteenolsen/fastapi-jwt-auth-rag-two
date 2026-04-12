6beb09e7f498c9fb15ea133f0182969dffae63f9
Per Olsen <persteen22@gmail.com>
2026-04-12 10:29:02 +0200

    Updated the api.py and tested the DevOps

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if not HF_TOKEN:
 # APP
 # -----------------------------
 app = FastAPI(
-    title="Python + FastApi + JWT Auth + LLM + Groq + RAG Pipeline",
+    title="Python + FastApi + JWT Auth + LLM + RAG Pipeline",
     description="12-04-2026 - FastAPI with JWT Auth serving an RAG Application powered by Groq + HuggingFace embeddings",
     version="0.0.2",
     contact={
